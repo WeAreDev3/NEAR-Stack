f5a996c0935dd7d29fa0f8c977da6716e3e95cbe
Jesse Smick <janka102@gmail.com>
2014-04-25 00:48:03 -0700

    Added swig "hello, world"

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express'),
+    swig = require('swig'),
 	app = express(),
 	config = require('./config');
 
+// Assign swig.renderFile to all .swig files
+app.engine('swig', swig.renderFile);
+
+// Set default extension to .swig
+app.set('view engine', 'swig');
+app.set('views', __dirname + '/views');
+
 app.get('/', function (req, res) {
-	res.send('Hello, world!');
+	res.render('index', { title: 'NEAR Stack' });
 });
 
 app.listen(config.port);
