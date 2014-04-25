e18f6fa0234e1963ce0d351b612369255baacbd7
Jesse Smick <janka102@gmail.com>
2014-04-25 10:37:50 -0700

    moved server files into server folder

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express'),
     swig = require('swig'),
-	app = express(),
-	config = require('./config');
+    app = express(),
+    config = require('./config');
 
 // Assign swig.renderFile to all .swig files
 app.engine('swig', swig.renderFile);
 
 // Set default extension to .swig
 app.set('view engine', 'swig');
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/server/views');
 
 app.get('/', function (req, res) {
-	res.render('index', { title: 'NEAR Stack' });
+    res.render('index', { title: 'NEAR Stack' });
 });
 
 app.listen(config.port);
