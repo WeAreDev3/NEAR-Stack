fdb02c014fa91e115636f01842fb3933e53ab54b
Jesse Smick <janka102@gmail.com>
2014-07-15 11:57:08 -0700

    Added a devMode property, based on NODE_ENV

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
-// Fix compatibility issues between Swig and AngularJS
-require('swig').setDefaults({
-	'varControls': ['{=', '=}']
-})
-
-// Define all of the configurations that we want
 module.exports = {
 	port: process.env.PORT || 3000,
 	root: __dirname,
+    devMode: (process.env.NODE_ENV || 'development') === 'development',
 	appName: 'NEAR Stack'
-}
+};
