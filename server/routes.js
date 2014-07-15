e31b0a7d027abecdd15df5da694ae8bc3e53c13a
Jesse Smick <janka102@gmail.com>
2014-07-15 11:59:18 -0700

    Cleaned/fixed up and added render stuff

--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,25 @@
-var config = require('./config');
+var config = require('../config');
 
 module.exports = function(app) {
     // The homepage
-    app.route('/').get(function (req, res) {
-        res.render('index', { title: config.appName });
+    app.route('/').get(function(req, res) {
+        res.render('index', {
+            title: 'Home',
+            assets: {
+                js: ['angular/angular.min.js'],
+                css: ['normalize-css/normalize.css', 'main.css']
+            }
+        });
     });
-    
+
     // A catch-all (i.e. 404)
-    app.route('*').all(function (req, res) {
-        res.render('404', { title: 'NEAR Stack - 404'});
+    app.route('*').all(function(req, res) {
+        res.render('404', {
+            title: '404',
+            assets: {
+                js: ['angular/angular.min.js'],
+                css: ['normalize-css/normalize.css', 'main.css']
+            }
+        });
     });
 };
